483cfe4678d5f085ffa23b6477e936e063b14bd9
trainsn <shineng9664@gmail.com>
2021-06-29 10:32:33 -0400

    time-varying version

--- src/main.cpp
+++ src/main.cpp
@@ -22,6 +22,7 @@
 #define STBI_MSC_SECURE_CRT
 #define STB_IMAGE_WRITE_IMPLEMENTATION
 #include "stb_image_write.h"
+#include "snapshots.h"
 using namespace std;
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -254,16 +255,18 @@ int main(int argc, char **argv)
 {
     char filename[1024];
 	sprintf(filename, argv[1]);
-	fprintf(stderr, "%s\n", argv[1]); 
+	char timeid[1024];
+	sprintf(timeid, argv[2]);
+	int timestep;
+	sscanf(timeid, "%d", &timestep);
+	fprintf(stderr, "%s/%s\n", argv[1], snapshots[timestep].c_str()); 
 	
-	string filename_s = filename;
-	int pos_last_dot = filename_s.rfind(".");
-	string input_name = filename_s.substr(0, pos_last_dot);
-    int pos_first_dash = filename_s.find("_");
-	string fileid = filename_s.substr(0, pos_first_dash);
+	string input_name = filename;
+    int pos_first_dash = input_name.find("_");
+	string fileid = input_name.substr(0, pos_first_dash);
 	
 	char input_path[1024];
-	sprintf(input_path, "/fs/project/PAS0027/MPAS1/Inter/%s", filename);
+	sprintf(input_path, "/fs/project/PAS0027/MPAS2/Results/%s/%s", filename, snapshots[timestep].c_str());
 	//loadMeshFromNetCDF("D:\\OSU\\Grade1\\in-situ\\6.0\\output.nc");
 	//loadMeshFromNetCDF("D:\\OSU\\Grade1\\in-situ\\MPAS-server\\Inter\\0070_4.88364_578.19012_0.51473_227.95909_ght0.2_epoch420.nc");
 	loadMeshFromNetCDF(input_path);
@@ -379,7 +382,7 @@ int main(int argc, char **argv)
 
 		stbi_flip_vertically_on_write(1);
 		char imagepath[1024];
-		sprintf(imagepath, "/fs/project/PAS0027/MPAS1/Inter/%s/layer%d.png", fileid.c_str(), layer_id);
+		sprintf(imagepath, "/fs/project/PAS0027/MPAS2/Results/%s/t%s/layer%d.png", fileid.c_str(), timeid, layer_id);
 		float* pBuffer = new float[SCR_WIDTH * SCR_HEIGHT * 4];
 		unsigned char* pImage = new unsigned char[SCR_WIDTH * SCR_HEIGHT * 3];
 		glReadBuffer(GL_BACK);
---
+++ src/snapshots.h
@@ -0,0 +1,18 @@
+#include <string>
+
+using namespace std;
+
+const string snapshots[12] = {
+"output.0001-07-03_12.00.00.nc",
+"output.0001-07-06_00.00.00.nc",
+"output.0001-07-08_12.00.00.nc",
+"output.0001-07-11_00.00.00.nc",
+"output.0001-07-13_12.00.00.nc",
+"output.0001-07-16_00.00.00.nc",
+"output.0001-07-18_12.00.00.nc",
+"output.0001-07-21_00.00.00.nc",
+"output.0001-07-23_12.00.00.nc",
+"output.0001-07-26_00.00.00.nc",
+"output.0001-07-28_12.00.00.nc",
+"output.0001-07-31_00.00.00.nc"
+};
